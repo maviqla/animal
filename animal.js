9218a71aff6d7d618b46d7fa7508aa869358ccf3
Victoria Maria <victoria.prjt@gmail.com>
2021-11-24 05:00:00 -0300

    hm

---
+++ animal.js
@@ -0,0 +1,44 @@
+const header = document.querySelector ("header")
+const main = document.querySelector ("main")
+
+fillHeader();
+fillMain();
+
+function fillHeader() {
+    header.innerHTML= "<div class='header'> <h1 id='neonText'>FLAMINGOS</h1></div>";
+}
+function fillMain() {
+    main.innerHTML= "<div><h1 style='color: white;'>Melhor que o ganso, mais gracioso que a garça</h1><div style='color: white;' id='card'><img style='opacity: 0.7' src='flamingos1.jpg' width='300px' height='300px' /><div id='columntxt'><h4>Informações Gerais</h4><p>Espécie: Flamingo-americano</p><p>Nome Científico: Phoenicopterus ruber</p><p>Reino: Animalia</p><p>Filo: Chordata</p><p>Classe: Aves</p><p>Ordem: Phoenicopteriformes</p><p>Graciosidade: Incrível</p><p>Equilíbrio: Perfeito</p><p>Beleza: Fora desse mundo</p><p>Aerodinâmica: Baixa</p><p>Fonte: <a href='https://pt.wikipedia.org/wiki/Flamingo-americano'>Wikipedia</a>, e sensatez</p></div><div id='columntxt'><h4>Descrição</h4><p>Tida como uma das mais graciosas e estranhas aves da avifauna mundial, o flamingo é o resultado inesperado da adaptação aos meios aquáticos que frequenta. Ave pernalta, pode ultrapassar um metro e meio de altura. Em média, os machos são um pouco maiores e têm o pescoço mais comprido do que as fêmeas. A envergadura das asas dos flamingos varia entre os 140 e os 165cm. </p><p>Rezam as lendas que é a melhor peça de cenário de hotéis de filmes dos anos 80.</p></div></div></div>"
+}
+
+
+
+
+    
+    
+    
+    
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+     
+
+     
+        
+        
+        
+     
+
+
